bfa7cc148163a09be9d83cb4728060682bc813d1
Garrett Bassen <77715672+GarrettBassen@users.noreply.github.com>
2024-08-21 13:39:35 -0700

    Debug page added functionality
    Added extra functionality

--- src/main/java/com/ags/vr/controllers/DebugController.java
+++ src/main/java/com/ags/vr/controllers/DebugController.java
@@ -1,8 +1,9 @@
 package com.ags.vr.controllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 
 public class DebugController
@@ -10,6 +11,20 @@ public class DebugController
     // Variables
     @FXML
     private Button btn_submit;
+    @FXML
+    private CheckBox cb_format_DLP;
+    @FXML
+    private CheckBox cb_format_EP;
+    @FXML
+    private CheckBox cb_format_LP;
+    @FXML
+    private CheckBox cb_format_single;
+    @FXML
+    private CheckBox cb_medium_CD;
+    @FXML
+    private CheckBox cb_medium_cassette;
+    @FXML
+    private CheckBox cb_medium_vinyl;
     @FXML
     private TextField txt_album;
     @FXML
