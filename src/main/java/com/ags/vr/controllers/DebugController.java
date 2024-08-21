1f72f3ff256ababe08c5d277fcf2be658c7ee9f3
Garrett Bassen <77715672+GarrettBassen@users.noreply.github.com>
2024-08-21 13:32:18 -0700

    Added debug page
    Added debug page for database testing

---
+++ src/main/java/com/ags/vr/controllers/DebugController.java
@@ -0,0 +1,28 @@
+package com.ags.vr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class DebugController
+{
+    // Variables
+    @FXML
+    private Button btn_submit;
+    @FXML
+    private TextField txt_album;
+    @FXML
+    private TextField txt_band;
+    @FXML
+    private TextField txt_genre;
+    @FXML
+    private TextField txt_year;
+
+    // Methods
+    @FXML
+    void AddMedia(ActionEvent event)
+    {
+        // TODO MAKE SQL QUERY HERE
+    }
+}
