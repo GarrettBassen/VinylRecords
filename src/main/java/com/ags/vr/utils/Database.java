fc25968f07a4e40f2a1c26a8bad0659c1e1873c5
Purbleguy <145792278+Alexander-Anderson306@users.noreply.github.com>
2024-08-22 13:01:47 -0700

    genre_linker functionality
    Created a method in Database.
    
    createGenreLink: Takes a media object and a genre and connects them via their ids through the genre_linker table.

--- src/main/java/com/ags/vr/utils/Database.java
+++ src/main/java/com/ags/vr/utils/Database.java
@@ -172,6 +172,43 @@ public class Database
         return bool;
     }
 
+    /**
+     * Creates the connection between the media table and the genres table through an
+     * intermediary table genre_linker. Uses the IDs of media and genres to create the connection.
+     * @param media Media object that is to be connected with a genre.
+     * @param genre Name of Genre that will be connected to a media.
+     * @return True if the connection was successfully made, otherwise false.
+     * @throws SQLException
+     */
+    public boolean createGenreLink(Media media, String genre) throws SQLException
+    {
+        PreparedStatement stmt = null;
+        boolean bool = false;
+        String genreID = String.valueOf(DBHelper.StringHash(genre));
+        String mediaID = String.valueOf(media.getID());
+
+        try
+        {
+            stmt = con.prepareStatement("INSERT INTO genre_linker VALUES (?,?)");
+            stmt.setString(1,mediaID);
+            stmt.setString(2,genreID);
+            stmt.execute();
+            bool = true;
+        }
+        catch (SQLException e)
+        {
+            Graphical.ErrorPopup("Database Error", e.toString());
+        }
+        finally
+        {
+            if(stmt != null)
+            {
+                stmt.close();
+            }
+        }
+        return bool;
+    }
+
 
     /**
      * Insets a band into the database,
