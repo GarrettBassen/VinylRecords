a4629d87eff8e24db8aa33113bf11cf0432c8c92
Purbleguy <145792278+Alexander-Anderson306@users.noreply.github.com>
2024-08-25 18:33:51 -0700

    Inventory Template First Draft
    First Draft
    
    Created an inventory page template. Unfortunately I was unaware of how split panes work, so the scaling is all messed up.
    
    Changes or fixes are appreciated.

---
+++ src/main/java/com/ags/vr/controllers/InventoryController.java
@@ -0,0 +1,104 @@
+package com.ags.vr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class InventoryController {
+
+    @FXML
+    private TextField addGenre;
+
+    @FXML
+    private TextField bandDisplay;
+
+    @FXML
+    private Spinner<?> bfSpinner;
+
+    @FXML
+    private Spinner<?> bgSpinner;
+
+    @FXML
+    private Spinner<?> bpSpinner;
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private Spinner<?> ffSpinner;
+
+    @FXML
+    private TextField formatDisplay;
+
+    @FXML
+    private TextField genresDisplay;
+
+    @FXML
+    private Spinner<?> gfSpinner;
+
+    @FXML
+    private TextField input;
+
+    @FXML
+    private TextField invTotal;
+
+    @FXML
+    private TextField mediumDisplay;
+
+    @FXML
+    private TextField nameDisplay;
+
+    @FXML
+    private Spinner<?> pfSpinner;
+
+    @FXML
+    private TextField removeGenre;
+
+    @FXML
+    private SplitPane split;
+
+    @FXML
+    private Button stockSave;
+
+    @FXML
+    private TextField upBand;
+
+    @FXML
+    private Text upFormat;
+
+    @FXML
+    private Text upMedium;
+
+    @FXML
+    private TextField upName;
+
+    @FXML
+    private TextField upYear;
+
+    @FXML
+    private Button update;
+
+    @FXML
+    private TextField yearDisplay;
+
+    @FXML
+    void applyUpdate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mediaSearch(ActionEvent event) {
+
+    }
+
+    @FXML
+    void stockSave(ActionEvent event) {
+
+    }
+
+}
+
