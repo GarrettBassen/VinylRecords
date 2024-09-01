47f172c5d6e0003a686fa2c22e91ba2be67dbbf1
Garrett Bassen <77715672+GarrettBassen@users.noreply.github.com>
2024-08-31 23:40:15 -0700

    Browse page styling
    Added searchButton.png created by Stephen
    
    Modified old search bar and changed UI design to simplify code (thank god the beast is dead)
    
    Modified methods in BrowseController.java and removed old code

--- src/main/java/com/ags/vr/controllers/BrowseController.java
+++ src/main/java/com/ags/vr/controllers/BrowseController.java
@@ -1,74 +1,35 @@
 package com.ags.vr.controllers;
 
 import javafx.fxml.FXML;
-import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 
 public class BrowseController
 {
     // Variables
     @FXML
     private TextField txt_search;
-    @FXML
-    private Button btn_search;
 
     /**
-     * Executes when key is released or mouse selects text field.
-     */
-    @FXML
-    void CreateSpace()
-    {
-        // TODO MAKE BETTER
-        if (txt_search.getText().length() < 4 || !txt_search.getText(0,4).isBlank())
-        {
-            String str = "";
-            if (!txt_search.getText().isBlank())
-            {
-                str = txt_search.getText().trim();
-            }
-
-            txt_search.setText("    " + str);
-            txt_search.positionCaret(4);
-        }
-
-        if (txt_search.getCaretPosition() < 4)
-        {
-            txt_search.positionCaret(4);
-        }
-    }
-
-    /**
-     * Executes when enter key is typed and text field is activated.
+     * Executes when key is typed with text field activated.
      * @param event Event
      */
     @FXML
-    void InputValidation(KeyEvent event)
+    void InputAction(KeyEvent event)
     {
         if (event.getCode().equals(KeyCode.ENTER))
         {
             // TODO SEARCH
             System.out.println("Enter Pressed");
         }
-
-        // TODO PLEASE FIX THIS
-        // TODO MAY ERASE EXISTING STRING
-        if (txt_search.getText().length() < 4 || !txt_search.getText(0,3).isBlank())
-        {
-            txt_search.setText("    ");
-            txt_search.positionCaret(4);
-        }
     }
 
     /**
      * Executes when search button is pressed.
-     * @param event Event
      */
     @FXML
-    void SearchButton(ActionEvent event)
+    void SearchButton()
     {
         // TODO SEARCH
         System.out.println("Search Button Pressed");
