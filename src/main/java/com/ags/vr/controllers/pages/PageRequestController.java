78aacd620671e70f2bd5d34648833fbcc5dafa96
Stephen-Nugent <nugents1999@gmail.com>
2024-09-18 18:15:41 -0700

    Created new Java Class called Request (very similar to the Media Class)
    - Has constructors
    
    - Has getters and setters
    
    - Might need more methods...

--- src/main/java/com/ags/vr/controllers/pages/PageRequestController.java
+++ src/main/java/com/ags/vr/controllers/pages/PageRequestController.java
@@ -3,19 +3,12 @@ package com.ags.vr.controllers.pages;
 //imports
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 
 /**
---
+++ src/main/java/com/ags/vr/objects/Request.java
@@ -0,0 +1,134 @@
+package com.ags.vr.objects;
+
+//imports
+import com.ags.vr.utils.Graphical;
+import java.io.Serializable;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * The Request Object that'll be used in various ways...
+ */
+public class Request implements Serializable {
+
+    //variable declaration
+    private String customerName;
+    private String phoneNumber;
+    private String email;
+    private String date;
+    private String request;
+
+
+    //------------------ Constructors ------------------
+
+    /**
+     * Default constructor.
+     */
+    public Request() {}
+
+    /**
+     * Constructor to create a Request object with the following parameters.
+     * @param customerName is string holding the respected information.
+     * @param phoneNumber is string holding the respected information.
+     * @param email is string holding the respected information.
+     * @param date is string holding the respected information.
+     * @param request is string holding the respected information.
+     */
+    public Request(String customerName, String phoneNumber, String email, String date, String request) {
+
+        this.customerName = customerName;
+        this.phoneNumber = phoneNumber;
+        this.email =email;
+        this.date = date;
+        this.request = request;
+    }
+
+    /**
+     * Creates Request object from database. WARNING you must supply a valid result set (example result.next()).
+     * @param request Request ResultSet
+     */
+    public Request(ResultSet request)
+    {
+        try
+        {
+            this.customerName = request.getString("customer");
+            this.phoneNumber = request.getString("phone_number");
+            this.email = request.getString("email");
+            this.date = request.getString("date");
+            this.request = request.getString("request");
+        }
+        catch (SQLException e)
+        {
+            Graphical.ErrorPopup("Error creating request", String.format(
+                    "Error creating request in Request(ResultSet) | Request.java\n\nCode: %s\n%s",
+                    e.getErrorCode(), e.getMessage()
+            ));
+        }
+    }
+
+
+    //------------------ Setters ------------------
+
+    /**
+     * Method to set the customer name.
+     * @param customerName is a string that represents who is making a request.
+     */
+    public void setCustomerName(String customerName) { this.customerName = customerName; }
+
+    /**
+     * Method to set the phone number.
+     * @param phoneNumber is a string that represents one way of contacting the customer.
+     */
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
+    /**
+     * Method to set the email.
+     * @param email is a string that represents one way of contacting the customer.
+     */
+    public void setEmail(String email) { this.email = email; }
+
+    /**
+     * Method to set the date.
+     * @param date is a string that represents the date that the request was made/entered into the program.
+     */
+    public void setDate(String date) { this.date = date; }
+
+    /**
+     * Method to set the request that was made.
+     * @param request is a string that represents the title and format of the media being requested.
+     */
+    public void setRequest(String request) { this.request = request; }
+
+
+    //------------------ Getters ------------------
+
+    /**
+     * Method to return the customers name to the user.
+     * @return a String representing the name of who is making the request.
+     */
+    public String getCustomerName() { return customerName; }
+
+    /**
+     * Method to return one way to contact the customer.
+     * @return a String representing the phone number of the customer making the request.
+     */
+    public String getPhoneNumber() { return phoneNumber; }
+
+    /**
+     * Method to return one way to contact the customer.
+     * @return a String representing the email of the customer making the request.
+     */
+    public String getEmail() { return email; }
+
+    /**
+     * Method to return the date of the request.
+     * @return a String representing the date that the request was made/entered into the program.
+     */
+    public String getDate() { return date; }
+
+    /**
+     * Method to return the request that was made.
+     * @return a String representing the title and format of media that the customer requested.
+     */
+    public String getRequest() { return request; }
+}
