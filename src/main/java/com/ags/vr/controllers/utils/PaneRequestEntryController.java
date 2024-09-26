773c184d5c173f1683dd48c4e4dfadbed1fee3b6
Stephen-Nugent <nugents1999@gmail.com>
2024-09-25 20:27:51 -0700

    Here's the rest...
    Guess the setData method wasn't included in the last push. Try now :)

--- src/main/java/com/ags/vr/controllers/utils/PaneRequestEntryController.java
+++ src/main/java/com/ags/vr/controllers/utils/PaneRequestEntryController.java
@@ -1,26 +1,42 @@
 package com.ags.vr.controllers.utils;
 
 //imports
+import com.ags.vr.objects.Request;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Circle;
 
 
 public class PaneRequestEntryController {
 
     //declaring FXML variables for any Request Entries
-    @FXML private Label request;
+    @FXML private Label requestName;
     @FXML private Label date;
     @FXML private Label customerName;
     @FXML private Label edit;
     @FXML private ImageView deleteIcon;
     @FXML private Label phoneNumber;
     @FXML private Label email;
-    
+
+    //initialize Request Object
+    private Request request;
+
 
     //future methods...
+
+    public void setData(Request request)
+    {
+        // Reference variables
+        this.request = request;
+
+        // Display variables
+        //this.pane_content.setPrefWidth(Integer.MAX_VALUE);
+        this.requestName.setText(request.getRequest());
+        this.customerName.setText(request.getCustomerName());
+        this.date.setText(String.valueOf(request.getDate()));
+        this.phoneNumber.setText(String.valueOf(request.getPhoneNumber()));
+        this.email.setText(request.getEmail());
+    }
 
     /**
      * Method used to remove request from the database.
@@ -30,6 +46,5 @@ public class PaneRequestEntryController {
         //temporary console print
         System.out.println("Request Deleted...");
     }
-
 
 }
